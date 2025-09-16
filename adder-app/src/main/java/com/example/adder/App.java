d2bc5d4ac226164d7fd9b7b6b02da945efaac1ec
Sehaj0007 <kaursehajpreet0728@gmail.com>
2025-09-16 12:53:45 +0530

    Update App.java

--- adder-app/src/main/java/com/example/adder/App.java
+++ adder-app/src/main/java/com/example/adder/App.java
@@ -1,9 +1,13 @@
 package com.example.adder;
 
 public class App {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
     public static void main(String[] args) {
         int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
-        System.out.println("Sum is: " + (a + b));
+        System.out.println("Sum is: " + add(a, b));
     }
 }
