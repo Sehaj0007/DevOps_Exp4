a6c288c287a3186e9d191e317ee123f9472b20a1
Sehaj0007 <kaursehajpreet0728@gmail.com>
2025-09-16 12:54:17 +0530

    Update AppTest.java

--- adder-app/src/test/java/com/example/adder/AppTest.java
+++ adder-app/src/test/java/com/example/adder/AppTest.java
@@ -1,38 +1,27 @@
 package com.example.adder;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest {
+
+    @Test
+    public void testAddPositiveNumbers() {
+        assertEquals(15, App.add(5, 10));
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test
+    public void testAddNegativeNumbers() {
+        assertEquals(-15, App.add(-5, -10));
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void testAddWithZero() {
+        assertEquals(10, App.add(0, 10));
+    }
+
+    @Test
+    public void testAddMixedNumbers() {
+        assertEquals(5, App.add(10, -5));
     }
 }
